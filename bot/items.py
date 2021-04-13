7602a53275289fee5c52c8e4eed389fde811a096
AgricolaJKB <64554282+AgricolaJKB@users.noreply.github.com>
2021-04-13 12:56:01 +0200

    commit

--- bot/items.py
+++ bot/items.py
@@ -1,6 +1,12 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
 import scrapy
 
 class test_item(scrapy.Item):
+   id = scrapy.Field()
    title = scrapy.Field()
    subtitle = scrapy.Field()
    amt = scrapy.Field()
--- bot/pipelines.py
+++ bot/pipelines.py
@@ -1,14 +1,32 @@
-import psycopg2
+# Define your item pipelines here
+#
+# Don't forget to add your pipeline to the ITEM_PIPELINES setting
+# See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
 
+
+# useful for handling different item types with a single interface
+from itemadapter import ItemAdapter
+
+# for .env: appdotenv
+
+import mariadb
+import sys
+from env import env
 
 class TutorialPipeline(object):
 
     def open_spider(self, spider):
         hostname = 'localhost'
-        username = 'postgres'
-        password = 'password' # your password
-        database = 'postgres'
-        self.connection = psycopg2.connect(host=hostname, user=username, password=password, dbname=database)
+        username = env('DB_USERNAME')
+        password = env('DB_PASSWORD')  # your password
+        database = env('DB_DATABASE')
+        port = 3306
+        print(username)
+        try:
+            self.connection = mariadb.connect(host=hostname, user=username, password=password, database=database, port=port)
+        except mariadb.Error as e:
+            print(f"Error connecting to MariaDB Platform: {e}")
+            sys.exit(1)
         self.cur = self.connection.cursor()
 
     def close_spider(self, spider):
@@ -16,7 +34,12 @@ class TutorialPipeline(object):
         self.connection.close()
 
     def process_item(self, item, spider):
-        print('yay')
-        self.cur.execute("insert into main(title, subtitle, amt, region, min_level, betreffsjahr, erscheinungsdatum, eintragungsdatum) values(%s,%s, %s,%s, %s,%s, %s,%s)",(item['title'],item['subtitle'], item['amt'],item['region'], item['min_level'],item['betreffsjahr'], item['erscheinungsdatum'],item['eintragungsdatum']))
+        try:
+            self.cur.execute(
+            f"insert into {env('DB_TABLE')}(orig_id, title, subtitle, amt, region, min_level, betreffsjahr, erscheinungsdatum, eintragungsdatum, url) values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+            (item['id'], item['title'], item['subtitle'], item['amt'], item['region'], item['min_level'], item['betreffsjahr'],
+             item['erscheinungsdatum'], item['eintragungsdatum'], item['url']))
+        except mariadb.Error as e:
+            print(f"Error: {e}")
         self.connection.commit()
         return item
